c0904149f7e7a9a73afa61ec97dd39fb2271b496
Francisco Natale <francisco_natale@hotmail.com>
2026-05-31 02:12:39 -0300

    agregue de donde se levantan los parametros y archivos varios

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "simulacion.continuo"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("simulacion.continuo.Main")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
